3132040d575095777eda35c221d24781239878b9
Dmitriy Rovenskiy <arhitec@bk.ru>
2020-07-01 19:45:31 +0300

    Initial commit

---
+++ src/js/game.js
@@ -0,0 +1,25 @@
+class Game {
+    start() {
+      console.log('game started');
+    }
+}
+
+class GameSavingData {
+}
+
+function readGameSaving() {
+}
+
+function writeGameSaving() {
+}
+
+export class GameSavingData {
+}
+  
+export function readGameSaving() {
+}
+  
+export function writeGameSaving() {
+}
+import Character from './domain.js';
+export default Game;
